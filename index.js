f8f69431d55c6204c3c2c5952728fe755c0ae76e
Melissa Real <melissa.real@hotmail.com>
2019-03-30 19:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here:
+function driversWithRevenueOver(arr, value){
+ return arr.filter(person => person.revenue > value);
+}
+
+
+function driverNamesWithRevenueOver(arr, value){
+  const newArr = []
+   for (const person of arr)
+     if(person.revenue > value){
+      newArr.push(person.name);
+    }
+    return newArr;
+}
+
+function exactMatch(arr, obj){
+
+  return arr.filter(person => person.value === obj[0]);
+
+}
