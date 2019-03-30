857806930cf29f623740157daf37387638d7122c
Melissa Real <melissa.real@hotmail.com>
2019-03-30 20:53:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function exactMatchToList(arr, obj){
     for(const key in obj){
        if ((person[key]===obj[key]) === true){
        console.log(person);
-       newArr.push(person.name);
+       newArr.push(person[key]);
       }
     }    return newArr;
   });
