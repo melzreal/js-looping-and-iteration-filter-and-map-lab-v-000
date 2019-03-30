fb81fad97dec2e299f1bd19b792fa03894e748e1
Melissa Real <melissa.real@hotmail.com>
2019-03-30 20:23:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function exactMatch(arr, obj){
 
    return arr.map(function(person){
     for(const key in obj){
-    return person[key]===obj[key] ? person[key]) : false ;
+     person[key]===obj[key] ? person[key] : false ;
     }
   });
 
