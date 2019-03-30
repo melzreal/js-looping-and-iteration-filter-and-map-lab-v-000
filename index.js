a92d8a5ebc4632849ae303d9d3c890190f162d35
Melissa Real <melissa.real@hotmail.com>
2019-03-30 19:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function driversWithRevenueOver(arr, value){
 
 function driverNamesWithRevenueOver(arr, value){
   const newArr = []
-  newArr = arr.filter(person => person.revenue > value);
-  return newArr;
+  return arr.filter(person =>
+    if (person.revenue > value){
+      newArr.push(person);
+    };);
 }
 
 function exactMatch(arr, obj){
